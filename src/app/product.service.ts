ea411a1145d060de0985edf277b966893527b48c
JMmmmm08 <buenaventurajohnmatthew@gmail.com>
2023-03-29 15:53:55 +0800

    Update product.service.ts

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,7 +1,7 @@
 
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
-
+import 'rxjs/add/operator/map';
 @Injectable()
 export class ProductService {
 
@@ -9,4 +9,7 @@ export class ProductService {
 
   constructor(private _http: Http) { }
 
+    getAlbum(id: number){
+      return this._http.get(this._albumUrl)
+    }
 }
